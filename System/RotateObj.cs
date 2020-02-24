d972e2262de5d1feb33d194ef9c657ca7913bff5
John C <edibleleadpaint@gmail.com>
2020-02-23 20:46:56 -0500

    reorganized files

---
+++ System/RotateObj.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObj : MonoBehaviour
+{
+
+	public Vector3 rotateSpeed;
+	
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Rotate(rotateSpeed * Time.deltaTime);
+	}
+}
